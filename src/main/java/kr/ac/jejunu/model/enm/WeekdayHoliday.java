1121fa7a37b89f113178d7b267ecd2fdd7c3acd1
Jeju-Boobby <boo1984@naver.com>
2017-10-10 20:04:15 +0900

    checkout

--- src/main/java/kr/ac/jejunu/model/enm/WeekdayHoliday.java
+++ src/main/java/kr/ac/jejunu/model/enm/WeekdayHoliday.java
@@ -1,8 +1,69 @@
 package kr.ac.jejunu.model.enm;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URLDecoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Boobby on 17-9-18.
  */
 public enum WeekdayHoliday {
-    weekday, holiday, everyday
+    weekday, holiday, everyday;
+
+    @Value("${eventDayApi.TDCProjectKey}")
+    private static String TDCProjectKey;
+
+    @Value("${eventDayApi.url}")
+    private static String url;
+
+    private static String LAST_DATE = "00000000";
+    private static WeekdayHoliday LAST_DAY  = WeekdayHoliday.weekday;
+
+    public static WeekdayHoliday whatDayToday() throws IOException, ParserConfigurationException, SAXException { // Great method naming....
+        Date today = new Date();
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+
+        String fullDate = dateFormat.format(today);
+
+        if (!LAST_DATE.equals(fullDate)) {
+            LAST_DATE = fullDate;
+
+            String year = fullDate.substring(0, 4);
+            String month = fullDate.substring(4, 6);
+            String day = fullDate.substring(6);
+
+            URI requestUrl = UriComponentsBuilder.newInstance()
+                    .scheme("http")
+                    .host(url)
+                    .queryParam("TDCProjectKey", URLDecoder.decode(TDCProjectKey, "UTF-8"))
+                    .queryParam("type", "")
+                    .queryParam("year", year)
+                    .queryParam("month", month)
+                    .queryParam("day", day)
+                    .build()
+                    .toUri();
+
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document document = documentBuilder.parse(requestUrl.toString());
+
+            document.getDocumentElement().normalize();
+
+            NodeList results = document.getEle
+        }
+    }
 }
