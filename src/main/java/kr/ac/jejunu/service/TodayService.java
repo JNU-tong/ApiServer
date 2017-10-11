d2f1b62042cbd038d77b79259a54506e562bb9d7
Jeju-Boobby <boo1984@naver.com>
2017-10-11 22:16:52 +0900

    today service 구현

--- src/main/java/kr/ac/jejunu/service/TodayService.java
+++ src/main/java/kr/ac/jejunu/service/TodayService.java
@@ -1,23 +1,26 @@
 package kr.ac.jejunu.service;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import kr.ac.jejunu.model.enm.WeekdayHoliday;
+import kr.ac.jejunu.model.response.EventDayInfo;
+import kr.ac.jejunu.model.response.EventDayJson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URLDecoder;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class TodayService {
@@ -31,60 +34,130 @@ public class TodayService {
     private String url;
 
     private String lastUpdatedDate;
+    private String today;
     private WeekdayHoliday lastDay;
 
     public TodayService() {
         lastUpdatedDate = "00000000";
         lastDay = WeekdayHoliday.holiday;
+        setToday();
+    }
+
+    public WeekdayHoliday whatDay(String yyyyMMdd) {
+        if (!isToday(yyyyMMdd)) {
+            lastUpdatedDate = yyyyMMdd;
+
+            if (isTodayHoliday() || isTodayWeekend())
+                lastDay = WeekdayHoliday.holiday;
+            else
+                lastDay = WeekdayHoliday.weekday;
+        }
+
+        return lastDay;
     }
 
     public WeekdayHoliday whatDayToday() {
-        if (isHoliday() || isWeekend())
-            return WeekdayHoliday.holiday;
-        else
-            return WeekdayHoliday.weekday;
-    }
-
-    private boolean isHoliday() {
-        // Today
-        Date today = new Date();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
-        String fullDate = dateFormat.format(today);
-
         // if last updated date equals today, will not check again.
-        if (!lastUpdatedDate.equals(fullDate)) {
-            try {
-                lastUpdatedDate = fullDate;
+        if (!isToday()) {
+            lastUpdatedDate = today;
 
-                String year = fullDate.substring(0, 4);
-                String month = fullDate.substring(4, 6);
-                String day = fullDate.substring(6);
-
-                URI requestUrl = UriComponentsBuilder.newInstance()
-                        .scheme("https")
-                        .host(url)
-                        .queryParam("TDCProjectKey", URLDecoder.decode(TDCProjectKey, "UTF-8"))
-                        .queryParam("type", "")
-                        .queryParam("year", year)
-                        .queryParam("month", month)
-                        .queryParam("day", day)
-                        .build()
-                        .toUri();
-
-                String response = restTemplate.getForObject(requestUrl, String.class);
-
-                System.out.println(response);
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
+            if (isTodayHoliday() || isTodayWeekend())
+                lastDay = WeekdayHoliday.holiday;
+            else
+                lastDay = WeekdayHoliday.weekday;
         }
-        return true;
+
+        return lastDay;
     }
 
-    private boolean isWeekend() {
+    private boolean isTodayHoliday() {
+        try {
+            String year = today.substring(0, 4);
+            String month = today.substring(4, 6);
+            String day = today.substring(6);
 
-        return true;
+            URI requestUrl = UriComponentsBuilder.newInstance()
+                    .scheme("https")
+                    .host(url)
+                    .queryParam("TDCProjectKey", URLDecoder.decode(TDCProjectKey, "UTF-8"))
+                    .queryParam("type", "")
+                    .queryParam("year", year)
+                    .queryParam("month", month)
+                    .queryParam("day", day)
+                    .build()
+                    .toUri();
+
+            String response = restTemplate.getForObject(requestUrl, String.class);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            EventDayJson eventDayJson = objectMapper.readValue(response, EventDayJson.class);
+
+            ArrayList<EventDayInfo> results = eventDayJson.getResults();
+
+            if (results != null) {
+                for (EventDayInfo result : results) {
+                    if (result.getType().contains("h") || result.getType().contains("i")) {
+                        lastDay = WeekdayHoliday.holiday;
+                        return true;
+                    }
+                }
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
 
+    private boolean isTodayWeekend() {
+        Calendar cal = Calendar.getInstance();
 
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+            Date todayDate = dateFormat.parse(today);
+            cal.setTime(todayDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        int weekNum = cal.get(Calendar.DAY_OF_WEEK);
+
+        if (weekNum == 1 || weekNum == 7)
+            return true;
+        else
+            return false;
+
+    }
+
+    private boolean isToday() {
+        setToday();
+
+        return lastUpdatedDate.equals(today);
+    }
+
+    private boolean isToday(String yyyyMMdd) {
+        setToday(yyyyMMdd);
+
+        return lastUpdatedDate.equals(yyyyMMdd);
+    }
+
+    private void setToday(String yyyyMMdd) {
+        today = yyyyMMdd;
+    }
+
+    private void setToday() {
+        Date todayDate = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+        today = dateFormat.format(todayDate);
+    }
+
+    public String getToday() {
+        return today;
+    }
 }
--- src/test/java/kr/ac/jejunu/service/TodayServiceTest.java
+++ src/test/java/kr/ac/jejunu/service/TodayServiceTest.java
@@ -1,12 +1,16 @@
 package kr.ac.jejunu.service;
 
+import kr.ac.jejunu.model.enm.WeekdayHoliday;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import java.text.ParseException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -14,8 +18,23 @@ public class TodayServiceTest {
     @Autowired
     private TodayService todayService;
 
-    @Test
-    public void test() {
-        todayService.whatDayToday();
-    }
+//    @Test
+//    public void test1() throws ParseException {
+//        todayService.whatDay("20171005");
+//        assertThat(todayService.getToday(), is("20171005"));
+//        assertThat(todayService.whatDayToday(), is(WeekdayHoliday.holiday));
+//    }
+//
+//    @Test
+//    public void test2() throws ParseException {
+//        todayService.whatDayToday();
+//        assertThat(todayService.whatDayToday(), is(WeekdayHoliday.weekday));
+//    }
+//
+//    @Test
+//    public void test3() throws ParseException {
+//        todayService.whatDay("20171014");
+//        assertThat(todayService.getToday(), is("20171014"));
+//        assertThat(todayService.whatDayToday(), is(WeekdayHoliday.holiday));
+//    }
 }
