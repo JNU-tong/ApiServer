c16f5d2085233f0036519cd1f2e1bda2e71633fd
Jeju-Boobby <boo1984@naver.com>
2017-10-29 23:50:13 +0900

    edit

---
+++ src/main/java/kr/ac/jejunu/service/jnu_bus/JnuBusScheduleService.java
@@ -0,0 +1,15 @@
+package kr.ac.jejunu.service.jnu_bus;
+
+import kr.ac.jejunu.repository.JnuBusScheduleRepository;
+import kr.ac.jejunu.service.generator.RemainTimeGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class JnuBusScheduleService {
+    @Autowired
+    private JnuBusScheduleRepository jnuBusScheduleRepository;
+
+    @Autowired
+    private RemainTimeGenerator remainTimeGenerator;
+
+    public
+}
